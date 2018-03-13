eb861b801477785d8f52d06b09536bbabe118093
logonpf <logonpf@fiap.com.br>
2018-03-12 22:41:42 -0300

    JPA DAO Genérico Teste

--- 05-JPA-DesignPatterns/src/br/com/fiap/jpa/teste/Teste.java
+++ 05-JPA-DesignPatterns/src/br/com/fiap/jpa/teste/Teste.java
@@ -2,6 +2,12 @@ package br.com.fiap.jpa.teste;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+
+import br.com.fiap.jpa.dao.AlunoDAO;
+import br.com.fiap.jpa.dao.impl.AlunoDAOImpl;
+import br.com.fiap.jpa.entity.Aluno;
+import br.com.fiap.jpa.exception.CommitException;
+import br.com.fiap.jpa.exception.KeyNotFoundException;
 import br.com.fiap.jpa.singleton.EntityManagerFactorySingleton;
 
 public class Teste {
@@ -12,9 +18,36 @@ public class Teste {
 		EntityManager em = fabrica.createEntityManager();
 		
 		//Cadastrar
+		Aluno aluno = new Aluno("rm22222", "Oscar");
+		AlunoDAO dao = new AlunoDAOImpl(em);
+		
+		try {
+			dao.inserir(aluno);
+			dao.commit();
+		} catch (CommitException e) {
+			e.printStackTrace();
+		}
+		
 		//Buscar
+		Aluno busca = dao.pesquisar(aluno.getRm());
+		System.out.println(busca.getNome());
+		
 		//Atualizar
+		busca.setNome("Alexandre");
+		try {
+			dao.atualizar(busca);
+			dao.commit();
+		} catch (CommitException e) {
+			e.printStackTrace();
+		}
+		
 		//Remover
+		try {
+			dao.remover(busca.getRm());
+			dao.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		em.close();
 		fabrica.close();
